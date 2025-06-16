aee678d41ffa78deaaf1cc2610d4fe7ca238ac84
Anshuman Garg <anshumangarg5410@gmail.com>
2025-06-16 10:30:30 +0530

    design changes

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
             <div className="inputbox bg-red-400 h-full w-[70%] rounded-[40px]">
               <input
                 type="text"
-                className="bg-red-400 outline-none w-full h-full ml-5 rounded-l-[40px] text-white font-semi-bold"
+                className="bg-red-400 outline-none w-full h-full ml-5 rounded-l-[40px] text-white font-semi-bold "
               ></input>
             </div>
             <div className="add w-[30%] h-[100%] cursor-pointer rounded-r-[40px] bg-red-500 font-semibold font-sans text-lg flex justify-center items-center text-white">
@@ -32,8 +32,8 @@ function App() {
           {/* task1 */}
           <div className="task h-[50px] w-full bg-green-500 rounded-lg flex justify-evenly items-center ">
             <label class="inline-flex items-center cursor-pointer">
-              <input type="checkbox" class="peer hidden w-[10%]" />
-              <div class="w-5 h-5 rounded-full border-2 border-red-400 peer-checked:bg-blue-600 peer-checked:border-blue-600 transition-all"></div>
+              <input type="checkbox" class="peer hidden w-[10%] " />
+              <div class="w-5 h-5 rounded-full border-2 border-red-400 peer-checked:bg-blue-600  peer-checked:border-blue-600 transition-all"></div>
             </label>
             <input
               type="text"
